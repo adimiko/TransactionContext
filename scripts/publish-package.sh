4d6c070e18e1c188fc41eadd29103efe26db72de
adimiko <amikolajczyk.it@gmail.com>
2024-02-05 04:33:39 +0100

    ci/cd

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -2,9 +2,9 @@
 echo Executing after success scripts on branch $GITHUB_REF_NAME
 echo Triggering Nuget package build
 
-cd $1
+cd source/$1
 dotnet pack -c release /p:PackageVersion=$GITHUB_REF_NAME --no-restore -o .
 
-echo Uploading package to Nuget using branch $GITHUB_REF_NAME
+echo Uploading $1 package to Nuget using branch $GITHUB_REF_NAME
 
 dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
